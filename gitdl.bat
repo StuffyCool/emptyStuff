d316e4d97b59b7c3691cf26ea889609a0fc4f3d6
StuffyCool <128089550+StuffyCool@users.noreply.github.com>
2024-07-05 05:41:17 +0300

    Create gitdl.bat

---
+++ gitdl.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+
+REM Change directory to the current directory where this batch file is located
+cd /d %~dp0
+
+REM Delete the .git directory and its contents
+rd /s /q .git
+
+REM Delete the batch script itself
+del "%~f0"
